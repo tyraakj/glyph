01368cfb4b8e317b54f15a1f52668f7b40a8cfe6
tyraakj <tyra191712@gmail.com>
2026-08-10 10:39:51 +0530

    feat(db): setup prisma schema and client

---
+++ lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// Check if we should use adapter-pg or accelerate
+const useAdapter = !connectionString.startsWith("prisma+postgres://");
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient(useAdapter ? { adapter } : undefined);
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
